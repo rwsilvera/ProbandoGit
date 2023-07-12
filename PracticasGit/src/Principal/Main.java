76702f80aafa4b5c12cad73db4d7cbe9a151246b
fito_ <fito_@Fito>
2023-07-12 16:32:34 -0300

    Trabajo desde laptops

--- PracticasGit/src/Principal/Main.java
+++ PracticasGit/src/Principal/Main.java
@@ -1,9 +1,18 @@
 package Principal;
 
+import java.sql.Date;
+
+import Entidades.Departamento;
+import Entidades.Persona;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//Date fechaNac = new Date(90,7,27);
+
+	//	Persona personaRodolfo = new Persona("Rodolfo", "Silvera", fechaNac, Departamento.LAVALLEJA);
+		
+		System.out.println("Saludamos a ");
 
 	}
 
